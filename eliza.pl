acc66c0603666e2f2e27928cdaa750d60ac8dccf
Jhuan Assumpcao <jhuanassumpcao@gmail.com>
2022-06-01 16:14:16 -0300

    Update eliza.pl

--- eliza.pl
+++ eliza.pl
@@ -1,18 +1,37 @@
 %%  eliza(+Stimuli, -Response) is det.
 %   @param  Stimuli is a list of atoms (words).
 %   @author Richard A. O'Keefe (The Craft of Prolog)
+verbo([amo], [ama]).
+verbo([amei], [amou]).
+verbo([tem], [tenho]).
+verbo([teve], [tinha]).
+verbos(X, Y) :- verbo(X, Y).
 
-name(
+:- dynamic nome/1.
+nome(lindo).
 
-eliza(Stimuli, Response) :-
+
+jereusa([meu, nome, eh, NomeNovo], Response) :-
+    nome(NomeAtual),
+    retract(nome(NomeAtual)),
+    assert(nome(NomeNovo)),
+    Response = [oi, NomeNovo],
+    !.
+
+jereusa(Stimuli, Response) :-
     template(InternalStimuli, InternalResponse),
     match(InternalStimuli, Stimuli),
     match(InternalResponse, Response),
     !.
 
-template([s([i,am]),s(X)], [s([why,are,you]),s(X),w('?')]).
-template([w(i),s(X),w(you)], [s([why,do,you]),s(X),w(me),w('?')]).
-template([w(i),s(X),w(you), w(eliza)], [s([i]),s(X),w(you),w(too)]).
+template([s([eu,estou]),s(X)], [s([porque, você, está]),s(X),w('?')]).
+template([w(eu),s(X),w(você)], [s([porque,voce]),w(me), s(Y),w('?')]) :-
+    verbos(X, Y).
+template([w(eu),s(X),w(você), w(jereusa)], [s([eu]),s(X),w(você),w(tambem), w(NomeNovo)]) :-
+    nome(NomeNovo).
+    
+template([s([quantos, anos, voce]),w(X), s(Z)], [w(eu),w(Y), w('18') ,w(anos), s(Z)]) :-
+    verbos(X, Y).
 
 
 
