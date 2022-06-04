4cb4ea929040de1e02b0cade4f9a1d08087ed9f2
Jhuan Assumpcao <jhuanassumpcao@gmail.com>
2022-06-03 22:36:19 -0300

    Update eliza.pl

--- eliza.pl
+++ eliza.pl
@@ -3,6 +3,14 @@
 %   @author Richard A. O'Keefe (The Craft of Prolog)
 
 
+/** <examplos>
+
+?- jereusa([eu, amo, você, jereusa], Response).
+?- jereusa([quais, sao, minhas, musicas, favoritas], Response).
+
+*/
+
+
 :- dynamic nome/1.
 nome(lindo).
 
@@ -20,25 +28,31 @@ jereusa(Stimuli, Response) :-
     match(InternalResponse, Response),
     !.
 
+
+
 template([s([eu, estou]), s(X)], [s([porque, você, está]),s(X),w('?')]).
-template([s([quantos, anos, voce]),s(X)], [w(eu),s(Y), w(18) ,w(anos)]) :-
+template([s([quantos, anos, voce]),s(X), s(_)], [w(eu),s(Y), w(18) ,w(anos)]) :-
     verbos(X, Y).
 template([s([o, que, voce]),s(X), s(_)], [w(eu),s(Y),s(W)]) :-
     verbos(X,Y),
 		respostas(X, W).
-template([w(eu),s(X),w(você)], [s([porque,voce]),w(me), s(Y),w('?')]) :-
+template([s(X),s(Z), s(_)], [s(Y),s(W),w(sim)]) :-
+    verbos(X,Y),
+    	respostas(Z,W).
+template([w(eu),s(X),w(voce)], [s([porque,voce]),w(me), s(Y),w('?')]) :-
     verbos(X, Y).
 template([w(quais),s(X),s(Y)], [s(X),s(Y), w(':'), s(W)]) :-
     respostas(Y,W).
 template([w(_),s(X),s(Y), s(K)], [s(X),s(Y),s(K), w(':'), s(W)]) :-
     respostas(Y,W).
-template([w(eu),s(X),w(você), w(jereusa)], [s([eu]),s(X),w(você),w(tambem), w(NomeNovo)]) :-
+template([w(eu),s(X),w(voce), w(jereusa)], [s([eu]),s(X),w(você),w(tambem), w(NomeNovo)]) :-
     nome(NomeNovo). 
 
-
-
+    
+   
 
 match([],[]).
+
 match([Item|Items],[Word|Words]) :-
     match(Item, Items, Word, Words).
 
@@ -60,6 +74,7 @@ verbo([amei], [amou]).
 verbo([tem], [tenho]).
 verbo([teve], [tinha]).
 verbo([tinha], [tinha]).
+verbo([namora], [namoro]).
 verbo([faz], [faço]).
 verbo([estudou], [estudei]).
 verbo([gosta], [gosto]).
@@ -68,22 +83,14 @@ palavra([gosta], [de, voce]).
 palavra([odeia], [humanos]).
 palavra([faz], [amizade]).
 palavra([ama], [voce]).
+palavra([comigo], [com, voce]).
 palavra([acha], [interessante]).
 palavra([feliz], [se, voce, esta, feliz, eu, tambem, estou]).
 palavra([estacoes], [verao, outono, inverno, primavera]).
 palavra([musicas], [anitta, envolver,luisa, sonsa, sentadao]).
 palavra([musica], [anitta, envolver]).
-pronome([sua], [minha]).
-pronome([suas], [minhas]).
 respostas(X, Y) :- palavra(X, Y).
-%respostas(X, Y) :- palavra(X, Y).
 verbos(X,Y) :- verbo(X,Y).
-pronomes(X,Y) :- pronome(X,Y).
 
 
-/** <examplos>
 
-?- jereusa([eu, amo, você, jereusa], Response).
-?- jereusa([i, love, you], Response).
-
-*/
