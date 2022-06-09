08d022909fa656d47d64a201f52da56649a1c2db
Matheus Machado <matheusmazar@gmail.com>
2022-06-09 20:09:50 -0300

    Update adventure.pl

--- adventure.pl
+++ adventure.pl
@@ -4,6 +4,8 @@
  * 
  */
 
+/*Game state and rules*/
+
 :- dynamic object_location/2.
 object_location(egg, pen).
 object_location(bazooka, woods).
@@ -18,21 +20,11 @@ creature_location(zombie, pen).
 connected(yard, pen).
 connected(yard, house).
 connected(yard, woods).
-is_connected(A, B) :-
-    connected(B, A);
-    connected(A,B).
 
 :- dynamic closed/2.
 closed(yard, pen).
 closed(yard, house).
 closed(yard, woods).
-is_closed(A, B) :-
-    closed(A, B);
-    closed(B, A).
-
-can_reach(A, B) :-
-    is_connected(A, B),
-    not(is_closed(A, B)).
 
 :- dynamic in_inventory/1.
 
@@ -44,42 +36,99 @@ can_open_a_door(paperclip).
 can_kill_creature(axe).
 can_kill_creature(bazooka).
 
+/*Useful predicates*/
+
+is_connected(A, B) :-
+    connected(B, A);
+    connected(A,B).
+
+is_closed(A, B) :-
+    closed(A, B);
+    closed(B, A).
+
+can_reach(A, B) :-
+    is_connected(A, B),
+    not(is_closed(A, B)).
+
+player_can_see_creature(Creature) :-
+    creature_location(Creature, Location),
+    player_location(Player),
+    can_reach(Location, Player).
+
+:- dynamic stopped/0.
+
+/*Base action predicates*/
+
+move_to(Destination) :-
+    player_location(Current),
+    not(creature_location(_, Destination)),
+  	can_reach(Current, Destination),
+  	retract(player_location(Current)),
+  	assert(player_location(Destination)).
+
+open(Destination, Object) :-
+    in_inventory(Object),
+    can_open_a_door(Object),
+    player_location(Player),
+    is_connected(Player, Destination),
+    is_closed(Player, Destination),
+    (retract(closed(Player, Destination)); retract(closed(Destination, Player))).
+
+get_object(Object) :-
+    player_location(CurrentLocation),
+    object_location(Object, CurrentLocation),
+    retract(object_location(Object, CurrentLocation)),
+    assert(in_inventory(Object)).
+
+kill(Object, Target) :-
+    can_kill_creature(Object),
+    player_location(PlayerLocation),
+    creature_location(Target, L),
+    can_reach(PlayerLocation, L),
+    retract(creature_location(Target, L)).
+
+ask_to_leave(Creature) :-
+    player_can_see_creature(Creature),
+    creature_location(Creature, L),
+    retract(creature_location(Creature, L)).
+
+act([go, to, Where]) :- move_to(Where).
+act([open, Door, with, Object]) :- open(Door, Object).
+act([get, Object]) :- get_object(Object).
+act([kill, Target, with, Object]) :- kill(Object, Target).
+act([can, you, leave, Creature]) :- ask_to_leave(Creature).
+
+/*I/O action predicates*/
 write_area(Current, LocationToWrite) :-
     is_closed(Current, LocationToWrite),
-    write(LocationToWrite), write(' [locked]'), nl,!.
-
+    write(LocationToWrite), write(' [locked]'), nl,
+    !.
 write_area(Current, LocationToWrite) :-
     is_connected(Current, LocationToWrite),
     creature_location(Creature, LocationToWrite),
-    write('The '), write(Creature), write(' is blocking the entrance to the '), write(LocationToWrite), nl, !.
-
+    write('The '), write(Creature), write(' is blocking the entrance to the '), write(LocationToWrite), nl,
+    !.
 write_area(Current, LocationToWrite) :-
     is_connected(Current, LocationToWrite),
-    write(LocationToWrite), nl,!.
-
+    write(LocationToWrite), nl,
+    !.
 write_area(_, _).
 
 write_areas :-
-    write('Areas:'),
-    nl,
+    write('Areas:'), nl,
     player_location(L),
     forall(is_connected(L, OtherLocation), write_area(L, OtherLocation)).
 
 where_am_i :-
-    write('You are in: '),
     player_location(X),
-    write(X),
-    nl.
+    write('You are in: '), write(X), nl.
 
-write_object(Object) :-
-    write(Object),
-    nl.
+write_object(Object) :- write(Object), nl.
 
 write_objects :-
     player_location(L),
     findall(Object, object_location(Object, L), []),
     !. 
-
 write_objects :-
     write('Objects: '),
     nl,
@@ -93,190 +142,105 @@ look_around :-
 
 inventory :-
     findall(Object, in_inventory(Object), []),
-    write('Inventory is empty'), nl.
-
+    write('Inventory is empty'), nl,
+    !.
 inventory :-
     write('Inventory: '), nl,
     forall(in_inventory(Object), write_object(Object)).
 
-get_object(Object) :-
+io_get_object(Object) :-
+    get_object(Object),
+    write('You obtained '), write(Object), nl,
+    !.
+io_get_object(Object) :-
     player_location(CurrentLocation),
     not(object_location(Object, CurrentLocation)),
-    write('This object is not in this area'),
-    nl,
+    write('You can\'t find '), write(Object), nl,
     !.
-
-get_object(Object) :-
+io_get_object(Object) :-
     in_inventory(Object),
-    write('You already have one of these'),
-    nl,
+    write('You already have one of these'), nl,
     !.
 
-get_object(Object) :-
-    player_location(CurrentLocation),
-    object_location(Object, CurrentLocation),
-    retract(object_location(Object, CurrentLocation)),
-    assert(in_inventory(Object)),
-    write('You obtained '), write(Object),
-    nl.
-
-move(Destination) :-
-    player_location(Current),
-  	can_reach(Current, Destination),
-  	retract(player_location(Current)),
-  	assert(player_location(Destination)).
-
-goto(X) :-
-    player_location(Y),
-    not(is_connected(Y, X)),
-  	write('You can\'t get there from here.'),
-    nl,
+io_move_to(Destination) :-
+    move_to(Destination),
+  	write('You are now in the '), write(Destination), nl,
     !.
-goto(X) :-
-    creature_location(_, X),
-    player_location(Y),
-    write_area(Y, X),
+io_move_to(Destination) :-
+    player_location(Player),
+    not(is_connected(Player, Destination)),
+  	write('You can\'t get there from here.'), nl,
     !.
-goto(X) :-
-    player_location(Y),
-    is_closed(X, Y),
+io_move_to(Destination) :-
+    player_location(Player),
+    is_closed(Player, Destination),
     write('The door is closed'), nl,
     !.
-goto(X) :-
-    move(X),
-  	write('You are in the '), write(X), nl,
+io_move_to(Destination) :-
+    creature_location(Creature, Destination),
+    write('The '), write(Creature), write(' is blocking the entrance to the '), write(Destination), nl,
     !.
 
-kill(Object, _) :-
-    invalid_object(Object), !.
-
-kill(Object, _) :-
-    not(can_kill_creature(Object)),
-    write('This object cannot kill anything'),
+io_kill(Object, Target) :-
+    kill(Object, Target),
+    write('You killed the '), write(Target), nl,
     !.
-
-kill(_, Target) :-
-    player_location(PlayerLocation),
-    creature_location(Target, L),
-    not(can_reach(PlayerLocation, L)),
-    write('You can\'t reach it'),
-    nl,
-    !.
-
-kill(_, Target) :-
-    not(creature_location(Target, _)),
-    write(Target),
-    write(' is not around'),
-    nl,
-    !.
-
-kill(_, Target) :-
-    creature_location(Target, L),
-    retract(creature_location(Target, L)),
-    write('You killed the '), write(Target),
-    nl.
-
-do_open(A, B) :-
-  	(retract(closed(A, B)); retract(closed(B, A))),
-	write('Opened!').
-
-invalid_object(Object) :-
+io_kill(Object, _) :-
     not(in_inventory(Object)),
     write('Object not in inventory'),
     !.
-
-try_open(Object, _, _) :-
-    invalid_object(Object),
+io_kill(Object, _) :-
+    not(can_kill_creature(Object)),
+    write(Object), write(' can\'t kill anything'),
+    !.
+io_kill(_, Target) :-
+    player_can_see_creature(Target),
+    write('You can\'t find the '), write(Target),
+    nl,
     !.
 
-try_open(Object, _, _) :-
-    not(can_open_a_door(Object)),
-    write('Object can\'t open a door'),
+io_open(Destination, Object) :-
+    open(Destination, Object),
+    write('The '), write(Destination), write(' is now opened'), nl,
     !.
-
-try_open(_, Current, Destination) :-
-    do_open(Current, Destination).
-
-open(Destination, _) :-
+io_open(Destination, _) :-
     player_location(Current),
     not(is_connected(Current, Destination)),
     write('This door doesn\'t exist'),
     !.
-
-open(Destination, _) :-
+io_open(Destination, _) :-
     player_location(Current),
     is_connected(Current, Destination),
     not(is_closed(Current, Destination)),
     write('This door is already opened'),
     !.
-
-open(Destination, Object) :-
-    player_location(Current),
-    is_connected(Current, Destination),
-    try_open(Object, Current, Destination),
+io_open(_, Object) :-
+    not(in_inventory(Object)),
+    write('Object not in inventory'),
+    !.
+io_open(_, Object) :-
+    not(can_open_a_door(Object)),
+    write(Object), write(' can\'t open a door'),
     !.
 
-:- dynamic stopped/0.
-
-ask_to_leave(Creature) :-
-    player_location(PlayerLocation),
-    creature_location(Creature, L),
-    not(can_reach(PlayerLocation, L)),
-    write('It can\'t hear you'),
-    nl,
+io_ask_to_leave(Creature) :-
+    ask_to_leave(Creature),
+    write('The '), write(Creature), write(' leaves, as requested'), nl,
     !.
+io_ask_to_leave(Creature) :-
+    write('You can\'t find the '), write(Creature), nl.
 
-ask_to_leave(Creature) :-
-    not(creature_location(Creature, _)),
-    write(Creature),
-    write(' is not around'),
-    nl,
-    !.
-
-ask_to_leave(Creature) :-
-    creature_location(Creature, L),
-    retract(creature_location(Creature, L)),
-    write('The '), write(Creature), write(' leaves, as requested'),
-    nl.
-
-/*Adicionar aqui o predicado para pedir que a criatura deixe o local, utilizando ask_to_leave(Creature)*/
-
-act([go, to, Where]) :-
-    goto(Where),
-    !.
-
-act([open, Door, with, Object]) :-
-    open(Door, Object),
-    !.
-
-act([kill, Target, with, Object]) :-
-    kill(Object, Target),
-    !.
-
-act([show, inventory]) :-
-    inventory,
-    !.
-
-act([stop]) :-
-    assert(stopped),
-    write('Goodbye!'),
-    nl,
-    !.
-
-act([where, am, i]) :-
-    where_am_i,
-    !.
-
-act([look, around]) :-
-    look_around,
-    !.
-
-act([get, Object]) :-
-    get_object(Object),
-    !.
-
-act(_) :-
-    write('Invalid action').
+io_act([go, to, Where]) :- io_move_to(Where), !.
+io_act([open, Door, with, Object]) :- io_open(Door, Object), !.
+io_act([open, Door]) :- in_inventory(key), io_open(Door, key), !.
+io_act([get, Object]) :- io_get_object(Object), !.
+io_act([kill, Target, with, Object]) :- io_kill(Object, Target), !.
+io_act([can, you, leave, Creature]) :- io_ask_to_leave(Creature), !.
+io_act([show, inventory]) :- inventory, !.
+io_act([stop]) :- stop, !.
+io_act([where, am, i]) :- where_am_i, !.
+io_act([look, around]) :- look_around, !.
+io_act(_) :- write('Invalid action'), nl.
 
 read_atoms(Atoms) :-
     read_line_to_codes(user_input, Cs),
@@ -288,9 +252,14 @@ loop :- in_inventory(egg), write('Congratulations, you won!'), nl, !.
 loop :-
   	prompt(_, 'Type an action...'),
   	read_atoms(Action),
-  	act(Action),
+  	io_act(Action),
   	loop.
 
 start :-
     write('Get the egg'), nl,
     loop.
+
+stop :-
+    assert(stopped),
+    write('Goodbye!'),
+    nl.
